8a902a16a897052f4047692c579f0cc09c1b68f7
n-lagomarsini <lago.88@libero.it>
2014-03-26 18:59:47 +0100

    Modifications on the Peoplecalculator

--- peoplecalculator/src/main/java/it/geosolutions/peoplecalculator/Constant.java
+++ peoplecalculator/src/main/java/it/geosolutions/peoplecalculator/Constant.java
@@ -4,5 +4,6 @@ public class Constant {
 
 	public final static Integer DEFAULT_DISTANCE = new Integer(10);
 	public static final String DEFAULT_OUTPUT_DIR = "output";
+	public static final String DEFAULT_OUTPUT_ATTRIBUTION_REL = "REL_ATTRIB";
 	
 }
